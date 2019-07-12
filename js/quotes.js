164116ef98285de9e5e13119e96cd32c37f06194
DG-T <orione_90@hotmail.com>
2019-07-12 15:05:47 +0200

    Simplify HTML, implement js functionality

--- js/quotes.js
+++ js/quotes.js
@@ -1,41 +1,41 @@
-// create quotes with different fragments
-var createQuote;
-var Quotes = {
-    init: function (quoteStart, quoteMiddle, quoteEnd) {
-        this.quoteStart = quoteStart;
-        this.quoteMiddle = quoteMiddle;
-        this.quoteEnd = quoteEnd;
-    }, 
-    // choose quote randomly
-    describe: function () {
-        var start = Math.floor(Math.random() * (this.quoteStart.length));
-        var middle = Math.floor(Math.random() * (this.quoteMiddle.length));
-        var end = Math.floor(Math.random() * (this.quoteEnd.length));
-
-        //join random quote together, and return full random quote
-        createQuote = (this.quoteStart[start] + " " + this.quoteMiddle[middle] + 
-        " " + this.quoteEnd[end]);
-        return createQuote;
-    }
+// my variables containing quotes fragments
+var wellness = {
+    start: ["Eating", "Exercising", "Medxiting"],
+   middle: ["its great for", "will lead you", "can give you"],
+   end: ["to wellbeing", "be wise", "strength"]
 };
-
-
-// fragments for wellness quotes
-var wellness = Object.create(Quotes);
-wellness.init(
-    ["I am always", "looking for", "good things"],
-    ["Sometimes", "one moment", "change the world"],
-    ["Remember", "is possible", "beleive in change"]
-    );
-
-    //fragments for positivism quotes
-var positivism = Object.create(Quotes);
-positivism.init(
-    ["Be positive", "always", "more opportunity"],
-    ["Think happyly", "if you want", "Keep it up"],
-    ["Start trying", "beleving in", "possibilities"]
-    );
-
-
-
-       
+ 
+ var positivism = {
+    start: ["I am always", "Sometimes", "Remember"],
+    middle: ["looking for", "one moment", "is possible"],
+    end: ["good things", "change the world", "beleive in change"]
+};
+ 
+// function with for loop and condition statement for create quote
+ function createQuote() {
+     var number = document.getElementById("quoteNumber").value;
+   var quote="";
+     for (var i = 0; i < number; i++) {
+         if (document.getElementById("quoteNumber").value >= 6){
+          alert("Error: Number must be between 1 and 5!")
+          break;
+     } else if (document.getElementById("chooseOption").value==="wellness"){
+       sub = wellness;
+     } else if (document.getElementById("chooseOption").value==="positivism"){ 
+      sub = positivism;
+     }
+     quote += sub.start[Math.floor(Math.random()*sub.start.length)]+" "
+     +sub.middle[Math.floor(Math.random()*sub.middle.length)]+" "
+     +sub.end[Math.floor(Math.random()*sub.end.length)]+"!"
+     +"\n";
+   }
+       return quote;
+ }
+ 
+ // function to display quote
+ function displayQuote() {
+     var show = document.getElementById("diplay");
+     show.innerText=createQuote();
+ }
+ 
+ displayQuote();
